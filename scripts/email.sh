863ba6c4dff558b00346e442a02be64279c8f577
Monique Mezher <mmm5fd@virginia.edu>
2018-01-29 17:47:06 -0500

    Update email.sh
    Finds collision files and sends to user.

--- scripts/email.sh
+++ scripts/email.sh
@@ -3,7 +3,6 @@
 # Email bash script to email results during HashClash process.
 # Change $YOUREMAILHERE to whatever your email is. Ask for the username and password to use in this script!
 # User must have heirloom-mailx installed.
-
 # Email for sending.
 user=$1
 echo "$user"
@@ -14,8 +13,8 @@ echo "$pass"
 rec=$3
 echo "$rec"
 # Creates folder for Google certificate for use in email. This has been added to instructions rather than automated.
-# certs="~/.certs"
-# googcerts="~/.certs/gmail.crt"
+certs="~/.certs"
+googcerts="~/.certs/gmail.crt"
 # count=100
 #if [ "$count" -eq 10 ]
 #then
@@ -26,4 +25,4 @@ echo "$rec"
 #  openssl s_client -connect smtp.gmail.com:465 | sed -ne '/-BEGIN CERTIFICATE-/,/-END CERTIFICATE-/p' > "~/.certs/gmail.crt" certutil -A -n "Google Internet Authority" -t "C," -d "~/.certs" -i "~/.certs/gmail.crt"
 #fi
 # Actually sending emails through.
-echo "Attached are your collision files. Enjoy!" | mailx -v -s "Your Collision is Ready" -S smtp-use-starttls -S ssl-verify=ignore -S smtp-auth=login -S smtp=smtp://smtp.gmail.com:587 -S from="$user""(Hash User)" -S smtp-auth-user="$user" -S smtp-auth-password="$pass" -S ssl-verify=ignore -S nss-config-dir="$certs" $rec
+for i in `find -name '*.coll'`; do echo "Attached are your collision files. Enjoy!" | mailx -a $i -v -s "Your Collision is Ready" -S smtp-use-starttls -S ssl-verify=ignore -S smtp-auth=login -S smtp=smtp://smtp.gmail.com:587 -S from="$user""(Hash User)" -S smtp-auth-user="$user" -S smtp-auth-password="$pass" -S ssl-verify=ignore -S nss-config-dir="$certs" $PUTYOUREMAILHERE; done
